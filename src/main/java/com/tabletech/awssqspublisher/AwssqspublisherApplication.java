0fa11559a703e975e5006521143efa8aed2e2629
pankaj kumar sharma <pankaj.ks@pokkt.com>
2024-05-08 10:47:40 +0530

    init commit

---
+++ src/main/java/com/tabletech/awssqspublisher/AwssqspublisherApplication.java
@@ -0,0 +1,13 @@
+package com.tabletech.awssqspublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwssqspublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwssqspublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabletech/awssqspublisher/AwssqspublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabletech.awssqspublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssqspublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
